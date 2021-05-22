78daa61b3fe716b6edea35efea959a9552d1983d
Shasita <jainshasita@gmail.com>
2021-05-22 13:54:17 +0530

    shasita

--- sketch.js
+++ sketch.js
@@ -2,6 +2,10 @@ var cloud, cloud_img;
 var bg, bg_img;
 var bird_img, thunderCloud_img;
 var gameState ; 
+var obstaclesGroup;
+var gameOver, restart;
+var gameOver_img, restart_img;
+var score = 0;
 
 function preload(){
 
@@ -9,7 +13,8 @@ cloud_img = loadImage("Images/cloud.png");
 bg_img = loadImage("Images/bg 1.jpg");
 bird_img = loadImage("Images/bird.png");
 thunderCloud_img = loadImage("Images/storm cloud.png");
-
+gameOver_img = loadImage("Images/score bg 1.png");
+restart_img = loadImage("Images/replay button.png");
 }
 
 function setup() {
@@ -19,39 +24,69 @@ function setup() {
    bg.addImage(bg_img);
    bg.scale = 3;
   
-   cloud = createSprite(400, 200, 50, 50);
-   cloud.addImage(cloud_img);
-   cloud.scale = 0.3;
+   obstaclesGroup = new Group ();
 
    gameState = 'play';
+
+   gameOver = createSprite(width/2, height/2);
+   gameOver.addImage(gameOver_img);
+   gameOver.visible = false;
+
+  restart = createSprite(width/2, height/2);
+  restart.addImage(restart_img);
+  restart.scale = 0.5;
+  restart.visible = false;
+
+  cloud = createSprite(400, 200, 50, 50);
+  cloud.addImage(cloud_img);
+  cloud.scale = 0.3;
+  cloud.setCollider('circle', 0, 0, 200);
 }
 
 function draw() {
   background("lightblue");  
 
   if(gameState === 'play'){
+    bg.velocityX = -4;
 
+    if(bg.x < 0){
+      bg.x = width/2;
+    }
+
+    cloud.velocityY = 4;
+
+    if(keyDown('space')){
+  
+      cloud.velocityY = -4;
+    }
+  score = score + Math.round(getFrameRate()/60);
+
+    spawnObstacles();
+
+    if(obstaclesGroup.isTouching(cloud)){
+      gameState = 'end';
+    }
   }
 
   else if(gameState === 'end'){
+    bg.velocityX = 0;
+    cloud.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+
+    obstaclesGroup.setLifetimeEach (-1);
+
+    gameOver.visible = true;
+    restart.visible = true;
 
   }
 
-  bg.velocityX = -4;
-
-  if(bg.x < 0){
-    bg.x = width/2;
-  }
-
-  cloud.velocityY = 4;
-
-  if(keyDown('space')){
-
-    cloud.velocityY = -4;
-  }
-
-  spawnObstacles();
+ 
   drawSprites();
+
+  fill ("magenta");
+  textSize (30);
+  textAlign(CENTER);
+  text (score, width-100, 50);
 }
 
 function spawnObstacles(){
@@ -66,16 +101,22 @@ function spawnObstacles(){
     switch(rand){
       case 1: obstacle.addImage(bird_img);
       obstacle.scale = 0.2;
+      obstacle.setCollider('rectangle', 0, 0, 350, 200);
       break;
 
       case 2: obstacle.addImage(thunderCloud_img);
       obstacle.scale = 0.5;
+      obstacle.setCollider('circle', 0, 0, 120);
       break;
 
       default: break;
     }
+
+    obstacle.lifetime = -Math.round(width/obstacle.velocityX);
+
+    obstaclesGroup.add(obstacle);
   }
 
-
+  
  
 }
